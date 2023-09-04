f24e6034af7854c6cf8e1657b3cec72664f9bf34
shreyabhandari0220 <115904795+shreyabhandari0220@users.noreply.github.com>
2023-09-04 15:27:54 -0700

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,11 +1,11 @@
 const config = {
-    apiKey: “AIzaSyDKr8r7o0vivu85fyRhn7bIn6j5GxljX8w”,
-    authDomain: “oatutor-askoski.firebaseapp.com”,
-    projectId: “oatutor-askoski”,
-    storageBucket: “oatutor-askoski.appspot.com”,
-    messagingSenderId: “1050614582892”,
-    appId: “1:1050614582892:web:f7ffec360148dc17da1249",
-    measurementId: “G-44V1SE1GEF”,
+    apiKey: "AIzaSyDKr8r7o0vivu85fyRhn7bIn6j5GxljX8w",
+    authDomain: "oatutor-askoski.firebaseapp.com",
+    projectId: "oatutor-askoski",
+    storageBucket: "oatutor-askoski.appspot.com",
+    messagingSenderId: "1050614582892",
+    appId: "1:1050614582892:web:f7ffec360148dc17da1249",
+    measurementId: "G-44V1SE1GEF",
 };
 
 export default config;
